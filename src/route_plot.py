96f09ac49684cac41869b6ee5274531ff8ea66a7
arshansani <arshansani@gmail.com>
2023-11-30 18:14:30 -0600

    Improve route_plot.py - Plot before and after next to each other - Set size for plots for consistancy

--- src/route_plot.py
+++ src/route_plot.py
@@ -1,23 +1,29 @@
 import json
 import matplotlib.pyplot as plt
 
-def plot_route(filename, title):
+def plot_route(filename, title, subplot_position):
     with open(filename, 'r') as file:
         data = json.load(file)
 
     x = [point['x'] for point in data]
     y = [point['y'] for point in data]
 
-    plt.figure()
+    plt.subplot(1, 2, subplot_position)
     plt.plot(x, y, marker='o')
     plt.title(title)
     plt.xlabel('X Coordinate')
     plt.ylabel('Y Coordinate')
     plt.grid(True)
-    plt.show()
+
+# Create a pre-sized figure for the plots
+plt.figure(figsize=(12, 6))
 
 # Plot the route before optimization
-plot_route('route_before_optimization.json', 'Route Before Optimization')
+plot_route('route_before_optimization.json', 'Route Before Optimization', 1)
 
 # Plot the route after optimization
-plot_route('route_after_optimization.json', 'Route After Optimization')
+plot_route('route_after_optimization.json', 'Route After Optimization', 2)
+
+# Show the plots
+plt.tight_layout()
+plt.show()
