46d75e1bb8c13479c67a223340b1233a21f95ac9
arshansani <arshansani@gmail.com>
2023-12-05 16:18:35 -0600

    Improve comment quality on route_simulation.cpp

--- src/route_simulation.cpp
+++ src/route_simulation.cpp
@@ -9,19 +9,22 @@ Address GenerateRandomAddress(int, int);
 int main() {
     std::srand(std::time(0)); // Seed for random number generation
 
+    // Delivery truck parameters
+    int truck_daily_deliveries = 100;
+    int num_trucks = 1;
+    // Address parameters
     int service_range = 20;
     int prime_latest_delivery_date = 1;
     int regular_latest_delivery_date = 14;
     int num_addresses = 1000;
     double prime_customers_percent = 0.3;
-
-    AddressList list;
-
     // Create a quantity of prime addresses and non prime addresses
     int num_prime_addresses = num_addresses * prime_customers_percent;
     int num_regular_addresses = num_addresses * (1 - prime_customers_percent);
 
+
     // Generate and add random addresses
+    AddressList list;
     for (int i = 0; i < num_prime_addresses; ++i) {
         Address random_address = GenerateRandomAddress(service_range, prime_latest_delivery_date);
         list.AddAddress(random_address);
@@ -31,34 +34,30 @@ int main() {
         list.AddAddress(random_address);
     }
 
+    // Initialize route so the starting end ending points are the depot
     Route route(list.GetAddresses());
 
     // Calculate initial total distance
     double initial_distance = route.TotalDistance();
     std::cout << "Initial total distance: " << initial_distance << std::endl;
-    // Save coordinates to a JSON-like file before and after optimization
     WriteRouteToJson(route.GetAddresses(), "route_before_optimization.json");
 
     // Greedy routing optimization
     route.GreedyRoute();
-    // Calculate optimized total distance
     double greedy_distance = route.TotalDistance();
     std::cout << "Greedy total distance: " << greedy_distance << std::endl;
-    // Save coordinates to a JSON-like file after optimization
     WriteRouteToJson(route.GetAddresses(), "route_after_greedy_search.json");
 
-    // Optimize the route
+    // Opt2 routing optimization
     route.OptimizeRoute();
-    // Calculate optimized total distance
     double optimized_distance = route.TotalDistance();
     std::cout << "Optimized total distance: " << optimized_distance << std::endl;
-    // Save coordinates to a JSON-like file after optimization
     WriteRouteToJson(route.GetAddresses(), "route_after_optimization.json");
 
     return 0;
 }
 
-// Function to generate a random address within the given range
+// Generate a random address
 Address GenerateRandomAddress(int range, int latest_delivery_date) {
     int x = (std::rand() % (2 * range + 1)) - range;
     int y = (std::rand() % (2 * range + 1)) - range;
@@ -66,6 +65,7 @@ Address GenerateRandomAddress(int range, int latest_delivery_date) {
     return Address(x, y, last_delivery_date);
 }
 
+// Write the list of addresses to a JSON file for plotting using python
 void WriteRouteToJson(const std::vector<Address>& addresses, const std::string& filename) {
     std::ofstream file(filename);
     if (!file.is_open()) {
